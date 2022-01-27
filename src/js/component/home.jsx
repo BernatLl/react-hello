0021a36095c9346b6ae4dbbafe3d7df62bb8949e
BernatLl <blloret@hotmail.com>
2022-01-27 08:52:51 +0000

    1up

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,14 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
+import "../../styles/index.css";
 //create your first component
 const Home = () => {
+	const [onColor, setonColor] = useState("red");
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="trafficlight">
+			<div className="light red switchOn"></div>
+			<div className="light yellow"></div>
+			<div className="light green"></div>
 		</div>
 	);
 };
