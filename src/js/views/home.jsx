ad9865634506aa5d5f5f8812e7677c0fad331a8c
BernatLl <blloret@hotmail.com>
2022-01-27 13:24:31 +0000

    ee

---
+++ src/js/views/home.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "../../styles/index.css";
+
+//create your first component
+const Home = () => {
+	const [glow, setGlow] = useState("off");
+	const glowOn = () => {
+		setGlow("on");
+	};
+	return (
+		<div className="trafficlight">
+			<div className={"light red glow"} onClick={glowOn}></div>
+			<div className={"light yellow glow"}></div>
+			<div className={"light green glow"}></div>
+		</div>
+	);
+};
+
+export default Home;
